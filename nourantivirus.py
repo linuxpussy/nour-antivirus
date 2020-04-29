1fc381706c55aef622e754ceca7d4fbf84a0c597
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-04-29 17:24:09 +0200

    Update nourantivirus.py

--- nourantivirus.py
+++ nourantivirus.py
@@ -2,6 +2,7 @@ import os
 import os.path
 from os import path
 def tura():
+    
     avir = num = input("Voulez vous updater les defentions[Y/n]")
     if avir = "Y":
      fgi = open("definitions.def","w+")
@@ -13,19 +14,21 @@ def tura():
       fgi = fgi.read
      else:
       fgi = fgi.write("Error.")
-      
+      path = os.getcwd()
+print ("The current working directory is %s" % path)
     fgi = open("ancetrigue.explore","w+")
-    fgi.write("io {"explore" + "eviprog"} io3")
-    print ("definitions.def : File exists: "+str(path.exists('definitions.def')))
+    fgi.write("io {exploreio + eviprog} io3")
+    print ("definitions.def : File exists:")
+    print ("path exist: "+str(path.exists('definitions.def')))
     print ("ancetrigue.explore : File exists: "+str(path.exists('ancetrigue.explore')))
     else:
         def stevonnie():
             stevonnie = 'Antivirus - Quarantine - Firewall'
             f'{stevonnie}'
-      
-      
-      
-    print ("directory exists:" + str(path.exists('Yuraireus')))
+            print ("directory exists:" + str(path.exists('Yuraireus')))
 def tronfusion():
     stevonnie = 'Antivirus - Quarantine - Firewall'
     f'{stevonnie}'
+    
+end = hex('0','9I0sçéru_t&y r_y')
+end
