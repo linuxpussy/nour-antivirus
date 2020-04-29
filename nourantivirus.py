caa80ff87d8fc93a48e600b03418a553a9568fc9
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-04-29 17:10:27 +0200

    Create nourantivirus.py

---
+++ nourantivirus.py
@@ -0,0 +1,31 @@
+import os
+import os.path
+from os import path
+def tura():
+    avir = num = input("Voulez vous updater les defentions[Y/n]")
+    if avir = "Y":
+     fgi = open("definitions.def","w+")
+     fgi.write("I9Jadèz tècgyhyh- yhjnhuyèuyè qhayè-yuçzsyu-t huCUIHÇ_D ZU_JU_ÈU")
+     fgi = open("definitions.def","a+")
+     fgi.write("FigxG YSGDHHHHHDEVukuihu")
+     fgi = open("definitions.def","r")
+     if fgi.mode == 'r':
+      fgi = fgi.read
+     else:
+      fgi = fgi.write("Error.")
+      
+    fgi = open("ancetrigue.explore","w+")
+    fgi.write("io {"explore" + "eviprog"} io3")
+    print ("definitions.def : File exists: "+str(path.exists('definitions.def')))
+    print ("ancetrigue.explore : File exists: "+str(path.exists('ancetrigue.explore')))
+    else:
+        def stevonnie():
+            stevonnie = 'Antivirus - Quarantine - Firewall'
+            f'{stevonnie}'
+      
+      
+      
+    print ("directory exists:" + str(path.exists('Yuraireus')))
+def tronfusion():
+    stevonnie = 'Antivirus - Quarantine - Firewall'
+    f'{stevonnie}'
